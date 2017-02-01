9395dbb9b3b0ea5fb36f02d3e8f6c6960ef93709
omarkhursheed <omarkhu@gmail.com>
2017-02-01 23:18:50 +0530

    Added further tests

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.utils import timezone
 # Create your views here.
 from django.views import generic
 from django.http import HttpResponseRedirect
@@ -11,12 +12,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five unpublished questions"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     mode = Question
